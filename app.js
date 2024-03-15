acbdd5acc7cb0595fdfcc185d304f80a082f2969
JosuePenuelas <josue.penuelas@uabc.edu.mx>
2024-03-15 12:48:53 -0700

    segundo commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//http://localhost:4000/
+const express = require('express')
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+const activoController = require('./controladores/activoController');
+const ubicacionController = require('./controladores/ubicacionController');
+const responsableController = require('./controladores/responsableController');
+
+app.get('/', (req, res) => {
+    res.send('Meta 3.1: Implementar un servicio web REST')
+});
+
+//activos
+app.get('/activos', activoController.obtenerDatos);
+app.get('/activos/id:id', activoController.obtenerPorId);
+app.get('/activos/Numserie:serie', activoController.obtenerPorNumSerie);
+app.post('/activos', activoController.agregarActivo);
+app.put('/activos/id:id', activoController.actualizarActivo);
+app.delete('/activos/id:id', activoController.eliminarActivo);
+
+//responsables
+app.get('/responsables', responsableController.obtenerDatos);
+app.get('/responsables/id:id', responsableController.obtenerPorId);
+app.get('/responsables/numEmpleado:num', responsableController.obtenerPorNumeroEmpleado);
+app.post('/responsables', responsableController.agregarResponsable);
+app.put('/responsables/id:id', responsableController.actualizarResponsable);
+app.delete('/responsables/id:id', responsableController.eliminarResponsable);
+
+//ubicaciones
+app.get('/ubicaciones', ubicacionController.obtenerDatos);
+app.get('/ubicaciones/id:id', ubicacionController.obtenerPorId);
+app.post('/ubicaciones', ubicacionController.agregarUbicacion);
+app.put('/ubicaciones/id:id', ubicacionController.actualizarUbicacion);
+app.delete('/ubicaciones/id:id', ubicacionController.eliminarUbicacion);
+
+app.listen(port, () => {
+    console.log('Servidor escuchando el puerto:', port)
+}).on('error', err => {
+    console.log('Error al iniciar el servidor:', err)
+});
---
+++ modelos/ubicacionModel.js
@@ -0,0 +1,47 @@
+let dbUbicaciones = [
+    { id: 0, descripcion: "Salon I", activosAsociados: [100, 150, 300], imagenUbi: "https://ingenieria.mxl.uabc.mx/pe_ico/images/Laboratorio/Laboratorios/salon.JPG" },
+    { id: 1, descripcion: "Salon A", activosAsociados: [200, 250], imagenUbi: "https://ingenieria.mxl.uabc.mx/pe_ico/images/Laboratorio/Laboratorios/Mac.JPG" },
+    { id: 2, descripcion: "AudioVisual", activosAsociados: [], imagenUbi: "https://comunicacioninstitucional.uabc.mx/sites/default/files/styles/max_325x325/public/espacios-uabc/salaaudiovisual122.jpg?itok=qAZwomuI" },
+]
+
+class Ubicacion {
+    constructor(id, descripcion, activosAsociados, imagenUbi) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.activosAsociados = activosAsociados;
+        this.imagenUbi = imagenUbi;
+    }
+}
+
+let findAll = function () {
+    return dbUbicaciones;
+}
+
+let findById = function (id) {
+    for (let i = 0; i < dbUbicaciones.length; i++) {
+        if (id == dbUbicaciones[i].id) {
+            const element = dbUbicaciones[i];
+            return element;
+        }
+    }
+}
+
+let agregar = function (ubicacion) {
+    dbUbicaciones.push(ubicacion)
+}
+
+let actualizar = function (id, nuevaUbicacion) {
+    const i = dbUbicaciones.findIndex(ubicacion => ubicacion.id == id);
+    dbUbicaciones[i] = nuevaUbicacion;
+}
+
+let eliminar = function (id) {
+    dbUbicaciones = dbUbicaciones.filter(ubicacion => ubicacion.id !== id);
+}
+
+exports.findAll = findAll;
+exports.findById = findById;
+exports.agregar = agregar;
+exports.actualizar = actualizar;
+exports.eliminar = eliminar;
+exports.Ubicacion = Ubicacion;
